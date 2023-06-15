a71ea4b73e8763d956b58ef0debf9ddbc9123f2c
CRIS-LI-DEV <131208714+CRIS-LI-DEV@users.noreply.github.com>
2023-06-15 15:06:48 -0400

    Spring Final

--- sprint_modulo_6/sprint6/aplicacion1/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from aplicacion1.models import PerfilAportador, PerfilBeneficiado,PerfilGestorFinanzas, Finanzas, Aporte,Beneficio
-# Register your models here.
-admin.site.register(PerfilBeneficiado)
-admin.site.register(PerfilAportador)
-admin.site.register(PerfilGestorFinanzas)
-admin.site.register(Finanzas)
-admin.site.register(Aporte)
-admin.site.register(Beneficio)
